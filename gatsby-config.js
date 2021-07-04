90b906c2f897850b6c40af937847b2c9f09501ac
Hengry <hengry@jubo.health>
2021-07-04 13:59:29 +0800

    try to fix ios safari manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,53 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         icon: 'src/favicons/asalato.svg',
+        icons: [
+          {
+            src: 'src/favicons/maskable_icon_48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'src/favicons/maskable_icon_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
         name: `Asalato Resolver`,
         short_name: `Asalato Resolver`,
         legacy: true,
-        icon_options: {
-          purpose: `any maskable`,
-        },
       },
     },
     // 'gatsby-plugin-offline',
