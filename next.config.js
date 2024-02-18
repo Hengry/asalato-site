b48ff675ec40786082451441e757f49b4b309964
Hengry <hengry1213@gmail.com>
2024-02-18 22:31:44 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withTwin = require('./withTwin.js');
 /** @type {import('next').NextConfig} */
 const nextConfig = withTwin({
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
+  },
   experimental: {
     serverActions: true,
   },
@@ -10,6 +14,8 @@ const nextConfig = withTwin({
 
 const withPWA = require('next-pwa')({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
 });
 
 module.exports = withPWA(nextConfig);
